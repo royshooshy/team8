9d729126b0e87265120e6a512c0b720884c50da9
royshooshy <roy.shooshy@gmail.com>
2021-10-08 23:03:13 +0300

    save status in employee-list

--- src/Component/list/List.tsx
+++ src/Component/list/List.tsx
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react";
 import ListItem from "../list-item/list-item";
 import { IListItem } from "../list-item/list-item.interface";
+import configData from "../../config.json";
+import axios from "axios";
 
 function List(props: any) {
-
-  const handleChange = (data: IListItem) => {
-   
-    console.log(data);
-  };
-
   const [listItems, setListItems] = useState<IListItem[]>([]);
-
   useEffect(() => {
-    setListItems(props.items);
+    setListItems(props.data.items);
   }, [props]);
+  const handleStatusChange = (data: IListItem) => {
+    props.data.onStatusChange(data);
+  };
   return (
     <div className="container">
       <div className="section">
@@ -27,8 +25,11 @@ function List(props: any) {
         </div>
         <div className="row columns is-multiline">
           {listItems.map((item: IListItem) => (
-            <ListItem key={item.id} data={{ item: { ...item }, onStatusChange: handleChange }}></ListItem>
-        ))}
+            <ListItem
+              key={item.id}
+              data={{ item: { ...item }, onStatusChange: handleStatusChange }}
+            ></ListItem>
+          ))}
         </div>
       </div>
     </div>
--- src/employees-list/employees-list.tsx
+++ src/employees-list/employees-list.tsx
@@ -8,6 +8,8 @@ import List from "../Component/list/List";
 import { IEmployeeStatus } from "./employee-status.interface";
 
 function EmployeesList() {
+  const [employees, setEmployees] = useState<IEmployee[]>([]);
+  const [listItems, setListItems] = useState<IListItem[]>([]);
   useEffect(() => {
     const getEmployyees = axios.get<IEmployee[]>(
       `${configData.SERVER_URL}/employees`
@@ -27,7 +29,6 @@ function EmployeesList() {
               // image
               selectedState: emp.status,
               states: statuses.data.map((s) => {
-               
                 return { id: s.id, value: s.value };
               }),
             };
@@ -37,12 +38,18 @@ function EmployeesList() {
     );
   }, []);
 
-  const [employees, setEmployees] = useState<IEmployee[]>([]);
-  const [listItems, setListItems] = useState<IListItem[]>([]);
-
+  const handleStatusChange = (data: IListItem) => {
+    const getEmployyees = axios.patch<any>(
+      `${configData.SERVER_URL}/employees/${data.id}`,
+      { status: data.selectedState }
+    );
+    getEmployyees.then((data) => {});
+  };
   return (
     <div>
-      <List items={[...listItems]}></List>
+      <List
+        data={{ items: [...listItems], onStatusChange: handleStatusChange }}
+      ></List>
     </div>
   );
 }
