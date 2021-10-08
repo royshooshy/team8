761bd7aa90a69b2b9a67c5d4e3939f05f13ef87c
royshooshy <roy.shooshy@gmail.com>
2021-10-08 22:09:41 +0300

    employee-list using list

--- src/Component/list-item/list-item.tsx
+++ src/Component/list-item/list-item.tsx
@@ -1,15 +1,88 @@
+import { useEffect, useState } from "react";
 import { IListItem } from "./list-item.interface";
 
 function ListItem(props: any) {
+  const [listItem, setListItem] = useState<IListItem>();
+  const [isStatusDDOpen, toggleStatusDD] = useState<boolean>(false);
+
+  useEffect(() => {
+    setListItem(props.data.item);
+  }, [props]);
   return (
-    <div>
-      {props.data.item.text}
-      <button onClick={onStatusChange}>Change Profile</button>
+    <div className="column is-4">
+      <div className="card">
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-96x96">
+                <img
+                  className="circled"
+                  src="https://avatars.dicebear.com/api/initials/john%20doe.svg"
+                  alt="Image"
+                />
+              </figure>
+            </div>
+            <div
+              onClick={() => toggleStatusDD(!isStatusDDOpen)}
+              className="media-content"
+            >
+              <span className="title is-4 no-padding">{listItem?.text}</span>
+
+              <div className="is-4">
+                <div
+                  className={`dropdown ${!isStatusDDOpen ? "" : "is-active"}`}
+                >
+                  <div className="dropdown-trigger">
+                    <button
+                      className="button"
+                      aria-haspopup="true"
+                      aria-controls="dropdown-menu3"
+                    >
+                      <span>
+                        {
+                          listItem?.states?.find(
+                            (s) => s.id === listItem.selectedState
+                          )?.value
+                        }
+                      </span>
+                      <span className="icon is-small">
+                        <i className="fas fa-angle-down" aria-hidden="true"></i>
+                      </span>
+                    </button>
+                  </div>
+                  <div className="dropdown-menu" role="menu">
+                    <div className="dropdown-content">
+                      {listItem?.states?.map(
+                        (state: { id: number; value: string }) => (
+                          <a
+                            key={state.id}
+                            onClick={(e) => onStatusChange(e, state)}
+                            href="#"
+                            className="dropdown-item"
+                          >
+                            {state.value}
+                          </a>
+                        )
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 
-  function onStatusChange() {
-    props.data.onStatusChange("XXXXXXXXXXXXXXXX");
+  function onStatusChange(e: any, state: { id: number; value: string }) {
+    e.preventDefault();
+    toggleStatusDD(!isStatusDDOpen);
+    setListItem({ ...listItem, selectedState: state.id } as IListItem);
+    props.data.onStatusChange({
+      ...listItem,
+      selectedState: state.id,
+    } as IListItem);
   }
 }
 
--- src/Component/list/List.tsx
+++ src/Component/list/List.tsx
@@ -3,7 +3,9 @@ import ListItem from "../list-item/list-item";
 import { IListItem } from "../list-item/list-item.interface";
 
 function List(props: any) {
+
   const handleChange = (data: IListItem) => {
+   
     console.log(data);
   };
 
@@ -13,12 +15,22 @@ function List(props: any) {
     setListItems(props.items);
   }, [props]);
   return (
-    <div>
-      {listItems.map((item: IListItem) => (
-        <ListItem
-          data={{ item: { ...item }, onStatusChange: handleChange }}
-        ></ListItem>
-      ))}
+    <div className="container">
+      <div className="section">
+        <div className="columns">
+          <div className="column has-text-centered">
+            <h1 className="title">Bulma Card Layout Template</h1>
+          </div>
+        </div>
+        <div className="column has-text-centered">
+          <h1 className="title">Bulma Card Layout Template</h1>
+        </div>
+        <div className="row columns is-multiline">
+          {listItems.map((item: IListItem) => (
+            <ListItem key={item.id} data={{ item: { ...item }, onStatusChange: handleChange }}></ListItem>
+        ))}
+        </div>
+      </div>
     </div>
   );
 }
